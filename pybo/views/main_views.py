b11953d73e703cee90ed8b97c137ef7e3d865d3d
이지훈 <free.jihun@gmail.com>
2024-08-03 06:26:46 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views/main_views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, P!'
+
+
+@bp.route('/')
+def index():
+    return redirect(url_for('question._list'))
+
+
+'''
+bp 객체 생성시 사용된 __name__은 모듈명인 'main_views'가 인수로 
+첫번째 인수로 전달한 "main"은 블루프린트의 별칭
+url 앞에 기본으로 붙일 접두어 url을 의미
+'''
